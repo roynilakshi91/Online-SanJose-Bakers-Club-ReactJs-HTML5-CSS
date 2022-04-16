f846f85260799351018a98ad3d364e082a069afd
Nilakshi Roy <82709191+roynilakshi91@users.noreply.github.com>
2022-04-16 03:18:44 -0700

    Create EventJson.js

---
+++ ClubReact/EventJson.js
@@ -0,0 +1,25 @@
+[{
+	"name": "Coffee Date",
+	"dates": ["March 10th", "March 24th", "April 28th", "May 26th",
+		"June 9th", "June 23rd", "July 7th", "July 21",
+		"July 28th", "August 4th", "etc..."
+	],
+	"description": "Tasting baked goods with a sip of coffee"
+}, {
+	"name": "Sharing recipes",
+	"dates": ["May 23rd", "June 20th", "July 25th",
+		"August 22nd", "September 19"
+	],
+	"description": "Everybody gathers and share their unique recipes"
+}, {
+	"name": "Writing Blogs",
+	"dates": ["Sundays in July, August, and September if conditions are appropriate"],
+	"description": "All the creative and winning recipes are added in the Creative bakers club blog"
+}, {
+	"name": "Meeting People",
+	"dates": ["March 10th", "March 24th", "April 28th", "May 26th",
+		"June 9th", "June 23rd", "July 7th", "July 21",
+		"July 28th", "August 4th", "etc..."
+	],
+	"description": "People of the community join together for an evening walk followed by a laughing class"
+}]
